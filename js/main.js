f7d7bf12bed9fc7bee2f3c547a7e97ababd5cc91
Benjamin Lestisha <lbenjamin@abiit.internal>
2017-11-07 10:38:54 -0400

    added a onclick function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-function changePicture() {
-      var myPopup = window.open("", "", "width=200, height=100");
-}
+function Facts() {
+               alert ("Bonus Facts about Antigua and Barbuda!");
+               document.write ("Antigua is 108 square miles and Barbuda is 62 square miles. Our Prime Minister is the Honourable Gaston Browne and our Governor General is the Honourable Sir Rodney Williams. There are 6 parishes: ST. John's, St. George's, St. Peter's, St. Philip's, St. Paul's and St. Mary's");
+           }
