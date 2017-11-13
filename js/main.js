7b43545d9c8106d682f36a8eaa1822ebf4dbabc7
Benjamin Lestisha <lbenjamin@abiit.internal>
2017-11-13 14:06:49 -0400

    Pefected the Google api code
    Inserted the markers as well

--- js/main.js
+++ js/main.js
@@ -10,4 +10,30 @@ alert ("click for more facts!!");
 
 }
 
-   
+function initMap() {
+    var location1 = {lat: 17.120214, lng:-61.84523};
+    var location2 = {lat: 17.125631,  lng: -61.820503};
+    var location3 = {lat: 17.121229, lng: -61.841054};
+    var location4 = {lat: 17.023955, lng: -61.775081};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: location1
+    });
+    var marker1 = new google.maps.Marker({
+      position: location1,
+      map: map
+    });
+    var marker2 = new google.maps.Marker({
+      position: location2,
+      map: map
+    });
+    var marker3 = new google.maps.Marker({
+      position: location3,
+      map: map
+    });
+    var marker4 = new google.maps.Marker({
+      position: location4,
+      map: map
+    });
+  }
+
